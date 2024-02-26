efbad0ec4efbb304c5a8f46aa36ef841be8a5907
andy <andyyroche@gmail.com>
2024-02-26 10:40:24 -0500

    loop

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,16 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+  
 
 for (let x = 0; x < topics.length; x++) {
  console.log(topics[x]);
 }
 
-/*
+function selectTopic() {
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -16,4 +22,4 @@ if (topic === 'HTML') {
 } else {
  console.log('Please try again!');
 }
-*/
+}
